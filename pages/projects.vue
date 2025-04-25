34986e64d30b5c5cddc2fb32c306b2288525d0b3
Shilpa <Shilpamishramiss@gmail.com>
2025-04-25 23:58:20 +0530

    update project icons

--- pages/projects.vue
+++ pages/projects.vue
@@ -80,7 +80,7 @@ import DevConfig from '~/developer.json';
 
 const config = ref(DevConfig)
 
-const techs = ['react', 'html', 'css', 'vue', 'angular', 'gatsby', 'flutter']
+const techs = ['react', 'html', 'css', 'vue', 'pandas', 'scikit-learn', 'matplotlib', 'tensorflow', 'openCV', 'numPy', 'java', 'javascript', 'node.js', 'next.js', 'keras']
 const filters = ref(['all'])
 const showFilters = ref(true)
 const projects = ref(config.value.projects)
